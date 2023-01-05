1798cf8d061310adff5f8b9f64f57c17b703e2d4
btjaden <btjaden@users.noreply.github.com>
2023-01-05 18:39:58 -0500

    Add files via upload

--- MLpronto.py
+++ MLpronto.py
@@ -1,17 +1,11 @@
+######################################
+#####   MLpronto version 1.0.1   #####
+######################################
+
 import os, sys, json, subprocess
 from inspect import getmembers, isfunction
 import mapper
 from notebook import outputNotebook
-
-
-#####################
-#####   USAGE   #####
-#####################
-
-if len(sys.argv) < 3:
-	sys.stderr.write("\nUSAGE: MLpronto.py <datafile> <parameters.json>" + "\n\n")
-	sys.stderr.write("MLpronto requires two command line arguments. MLpronto takes a data file (.csv, .tsv, .txt, .xls, .xlsx, .xlsm, .xlsb, .ods) and a JSON file (indicating ML parameter options) and it generates a file with ML code that can be executed to analyze the data file. Output is to a file with the same name as the input data file but with *x_* appended on the front and the file extension replaced with *.py*.\n\n")
-	sys.exit(1)
 
 
 
@@ -28,7 +22,32 @@ def verifyFile(filename):
 
 
 
-def generateCode(data_filename, blocks):
+def generateCode(data_filename, params):
+        # DEFINE SEQUENCE OF CODE BLOCKS TO EXECUTE
+        blocks = ['header',
+                  'libraries',
+                  'body',
+                  'preprocessing',
+                  'read_data',
+                  'remove_rows',
+                  'labels_contain_nan',
+                  'labels_to_y',
+                  'labels_non_numeric',
+                  'remove_cols',
+                  'encode_binary_features',
+                  'one_hot_encode_features',
+                  'features_to_X',
+                  'univariate_imputation',
+                  'multivariate_imputation',
+                  'split_data',
+                  'feature_scaling',
+                  'model',
+                  'train_and_predict',
+                  'evaluate',
+                  'output',
+                  'warnings',
+                  'random_seed']
+
         # Name of code file
         head, tail = os.path.split(data_filename)
         if (head.strip() != ''): head += os.sep
@@ -46,7 +65,7 @@ def generateCode(data_filename, blocks):
                 # Write body, e.g., code
                 for b in blocks: out_file.write(functions[b]('body', params))
 
-        sys.stdout.write('\n' + 'A file of ML code has been generated:\t' + code_filename + '\n')
+        outputNotebook(code_filename)  # Generate iPython notebook
         return code_filename
 
 
@@ -55,7 +74,7 @@ def executeCode(code_filename, data_filename):
         sys.stdout.write('Results of executing the code file...' + '\n')
         currentDir = os.getcwd()
         if (os.path.split(data_filename)[0] != ''): os.chdir(os.path.split(data_filename)[0])
-        p = subprocess.run(['python', os.path.split(code_filename)[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.run(['/opt/rh/rh-python36/root/usr/bin/python', os.path.split(code_filename)[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         os.chdir(currentDir)
 
         # Check if there was any error when executing the code file
@@ -67,8 +86,24 @@ def executeCode(code_filename, data_filename):
         else:
                 sys.stdout.write('\n' + p.stdout.decode() + '\n\n')
 
-        # Generate iPython notebook
-        outputNotebook(code_filename)
+
+
+def run_MLpronto(data_filename, parameter_filename):
+
+        # CHECK THAT FILES EXIST
+        data_filename = verifyFile(data_filename)
+        parameter_filename = verifyFile(parameter_filename)
+
+        # LOAD PARAMETER OPTIONS FROM JSON FILE
+        with open(parameter_filename, 'r') as f: params = json.load(f)  # Load parameters
+        params['filename_temp'] = os.path.split(data_filename)[1]  # Temp data file name
+
+        # GENERATE CODE
+        code_filename = generateCode(data_filename, params)
+        sys.stdout.write('\n' + 'A file of ML code has been generated:\t' + code_filename + '\n')
+
+        # EXECUTE CODE
+        executeCode(code_filename, data_filename)
 
 
 
@@ -76,41 +111,10 @@ def executeCode(code_filename, data_filename):
 ##########   MAIN   ##########
 ##############################
 
-# CHECK THAT FILES EXIST
-data_filename = verifyFile(sys.argv[1])
-parameter_filename = verifyFile(sys.argv[2])
-
-# LOAD PARAMETER OPTIONS FROM JSON FILE
-with open(parameter_filename, 'r') as f: params = json.load(f)  # Load parameters
-params['filename_temp'] = os.path.split(data_filename)[1]  # Temp data file name
-
-# DEFINE SEQUENCE OF CODE BLOCKS TO EXECUTE
-blocks = ['header',
-          'libraries',
-          'body',
-          'preprocessing',
-          'read_data',
-          'remove_rows',
-          'labels_contain_nan',
-          'labels_to_y',
-          'labels_non_numeric',
-          'remove_cols',
-          'encode_binary_features',
-          'one_hot_encode_features',
-          'features_to_X',
-          'univariate_imputation',
-          'multivariate_imputation',
-          'split_data',
-          'feature_scaling',
-          'model',
-          'train_and_predict',
-          'evaluate',
-          'output',
-          'random_seed']
-
-# GENERATE CODE
-code_filename = generateCode(data_filename, blocks)
-
-# EXECUTE CODE
-executeCode(code_filename, data_filename)
+if __name__ == '__main__':
+        if len(sys.argv) < 3:
+                sys.stderr.write("\nUSAGE: MLpronto.py <datafile> <parameters.json>" + "\n\n")
+                sys.stderr.write("MLpronto requires two command line arguments. MLpronto takes a data file (.csv, .tsv, .txt, .xls, .xlsx, .xlsm, .xlsb, .ods) and a JSON file (indicating ML parameter options) and it generates a file with ML code that can be executed to analyze the data file. Output is to a file with the same name as the input data file but with *x_* appended on the front and the file extension replaced with *.py*.\n\n")
+                sys.exit(1)
+        run_MLpronto(sys.argv[1], sys.argv[2])
 
